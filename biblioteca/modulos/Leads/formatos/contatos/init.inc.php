df5b495c0466174c96b0d1623f03cde83274dfda
Ubiratã <birah.carvalho@gmail.com>
2019-03-14 22:08:09 -0300

    Alterei Leads para Cadastros

--- biblioteca/modulos/Leads/formatos/contatos/init.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ( ! defined( '__ROOT_PATH' ) ) {
-    die("Nope,my friend! Wrong path.");
-}
-
-require_once __DIR__ . "/config.php";
-require_once __DIR__ . "/contatos.inc.php";
-require_once __DIR__ . "/formulario.inc.php";
