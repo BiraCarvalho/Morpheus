efec1fd58408dd812d362baf31a7514ea8b2f926
Ubiratã <bira@biracavalho.com.br>
2019-03-21 17:38:02 -0300

    Comentei algumas linhas

--- aplicacao/config/rotas.config.php
+++ aplicacao/config/rotas.config.php
@@ -4,8 +4,8 @@ return [
 
         //Site
         //"home"       => [ "Diretorio" => "Site", "Privado" => 0, "Uri" => __SITE_PATH . "/pagina.php",       "Titulo" => "Home"                         ],
-        "login"      => [ "Diretorio" => "Site", "Privado" => 0, "Uri" => __SITE_PATH . "/pagina.php",       "Titulo" => "Login"                        ],
-        "pagina"     => [ "Diretorio" => "Site", "Privado" => 0, "Uri" => __SITE_PATH . "/pagina.php",       "Titulo" => ""                             ],
+        //"login"      => [ "Diretorio" => "Site", "Privado" => 0, "Uri" => __SITE_PATH . "/pagina.php",       "Titulo" => "Login"                        ],
+        //"pagina"     => [ "Diretorio" => "Site", "Privado" => 0, "Uri" => __SITE_PATH . "/pagina.php",       "Titulo" => ""                             ],
         "construcao" => [ "Diretorio" => "Site", "Privado" => 0, "Uri" => __SITE_PATH . "/construcao.php",   "Titulo" => "Projeto em Desenvolvimento"   ],
         "manutencao" => [ "Diretorio" => "Site", "Privado" => 0, "Uri" => __SITE_PATH . "/manutencao.php",   "Titulo" => "Site em manutenção"           ],
         "404"        => [ "Diretorio" => "Site", "Privado" => 0, "Uri" => __SITE_PATH . "/404.php",          "Titulo" => "404 - Página não encontrada!" ],
