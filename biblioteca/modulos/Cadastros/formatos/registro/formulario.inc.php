36f2e34f5c22aae96caa3df61c56dbbdd090f4b4
Ubiratã <bira@biracavalho.com.br>
2019-04-12 19:52:25 -0300

    Renomei os id do header das páginas

--- biblioteca/modulos/Cadastros/formatos/registro/formulario.inc.php
+++ biblioteca/modulos/Cadastros/formatos/registro/formulario.inc.php
@@ -1,7 +1,7 @@
 <article>
 	
     <header class="d-none">
-        <h2 id="article--titulo">Registro</h2>
+        <h2 id="pagina--titulo">Registro</h2>
     </header>
 
     <form id="form-main-<?=$secao_modulo?>Registro" class="form--registro formato--form-main m-auto" method="post" >
