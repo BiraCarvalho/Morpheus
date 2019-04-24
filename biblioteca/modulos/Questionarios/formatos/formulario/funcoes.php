cbe9f120d9cb0fb1062f0673e8efa058d07a1237
Ubiratã <bira@biracavalho.com.br>
2019-04-23 22:52:27 -0300

    Removi funções e configurações não utilizadas

--- biblioteca/modulos/Questionarios/formatos/formulario/funcoes.php
+++ biblioteca/modulos/Questionarios/formatos/formulario/funcoes.php
@@ -1,19 +1,3 @@
 <?php
 
-// function questionarios__resultado(int $questionario_id){
-
-//     $consulta = "SELECT  
-//                     Perguntas.Agrupamento, 
-//                     SUM(Respostas.Valor) AS Soma, 
-//                     COUNT(Perguntas.Id) AS NumeroDePerguntas, 
-//                     SUM(Respostas.Valor)/COUNT(Perguntas.Id) AS Media
-//                     FROM QuestionariosPerguntas AS Perguntas 
-//                     JOIN QuestionariosRespostas AS Respostas ON Perguntas.Id = Respostas.QuestionariosPerguntasId
-//                     WHERE Perguntas.QuestionariosId = '{$questionario_id}'
-//                     GROUP BY Perguntas.Agrupamento
-//                     ORDER BY Perguntas.Agrupamento";
-
-//     return global__db()->fetchAll($consulta);
-// }
-
 
